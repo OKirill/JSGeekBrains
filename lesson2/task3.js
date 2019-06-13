261adec9b5db69a5ca75b546fddd264c662f8379
Okirill <hotasice41@gmail.com>
2019-06-13 13:40:34 +0200

    5 заданий выполнено

---
+++ lesson2/task3.js
@@ -0,0 +1,11 @@
+var a = 6
+var b = 4
+
+if(a >= 0 && b >= 0){
+    alert(a - b);
+
+}else if(a < 0 && b < 0){
+    alert(a * b);
+}else if((a >= 0 && b < 0) || (a < 0 && b >= 0) ){
+    alert(a + b);
+}
