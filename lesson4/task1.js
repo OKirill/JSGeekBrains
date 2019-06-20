e62a0ccdf74da9d49e6f8c0ed7a31e6387f898fa
Okirill <hotasice41@gmail.com>
2019-06-20 14:23:02 +0200

    исправления

--- lesson4/task1.js
+++ lesson4/task1.js
@@ -5,17 +5,17 @@ function numToObj(number) {
         tens: 0,
         hundreds: 0,
     };
-    if (digit.number <= 9){
+    if (digit.number <= 9) {
         digit.units = digit.number;
-    } else if (digit.number <= 999){
+    } else if (digit.number <= 999) {
         digit.units = Math.floor(digit.number % 10);
         digit.tens = Math.floor(digit.number / 10 % 10);
         digit.hundreds = Math.floor(digit.number / 100 % 10);
-    } else  {
-        
+    } else {
+
         console.log('Введите число от 0 - 999!');
     }
-  
+
     console.log(digit);
 }
 
