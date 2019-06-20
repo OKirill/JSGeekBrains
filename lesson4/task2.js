d7c8199a71c0e04087b4c4d5fa522159f9b36b9d
Okirill <hotasice41@gmail.com>
2019-06-20 14:19:36 +0200

    второе задание

---
+++ lesson4/task2.js
@@ -0,0 +1,15 @@
+
+var basket = {
+    hearphones: 325,
+    charger: 187,
+    usb: 220,
+    price: 0,
+    priceCounter: function (basket) {
+
+        basket.price = basket.hearphones + basket.charger + basket.usb
+        console.log("Общая сумма товаров: " + price);
+
+    }
+}
+
+
