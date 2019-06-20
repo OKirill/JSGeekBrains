3c7e6a7a44a3d8d5c358c5cd5c135c4179a5f91c
Okirill <hotasice41@gmail.com>
2019-06-20 14:37:38 +0200

    доделал 2 дз

--- lesson4/task2.js
+++ lesson4/task2.js
@@ -1,15 +1,14 @@
-
-var basket = {
+let basket = {
     hearphones: 325,
     charger: 187,
     usb: 220,
     price: 0,
     priceCounter: function () {
-
-        this.price = this.hearphones + this.charger + this.usb
-        console.log("Общая сумма товаров: " + price);
-
+  
+      this.price = this.hearphones + this.charger + this.usb
+      console.log("Общая сумма товаров: " + this.price);
+  
     }
-}
-
-
+  }
+  
+  basket.priceCounter();
